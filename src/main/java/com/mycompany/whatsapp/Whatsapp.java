fd4ac94846dc08208ee894891de71471d564ef10
Student <Student@LabVM2049939.xiulez5jvhfu1iulqpbsmedgvb.jnbx.internal.cloudapp.net>
2026-09-17 09:21:51 +0200

    initialize commit

---
+++ src/main/java/com/mycompany/whatsapp/Whatsapp.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.whatsapp;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Whatsapp {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = input.nextLine();
+
+        System.out.print("Enter South African cell number: ");
+        String cellPhoneNumber = input.nextLine();
+
+        System.out.print("Enter first name: ");
+        String firstName = input.nextLine();
+
+        System.out.print("Enter last name: ");
+        String lastName = input.nextLine();
+
+        Login user = new Login(username, password, cellPhoneNumber);
+
+        String registrationMessage = user.registerUser();
+        System.out.println(registrationMessage);
+
+        if (registrationMessage.equals("User has been registered successfully.")) {
+            System.out.print("Enter username to login: ");
+            String enteredUsername = input.nextLine();
+
+            System.out.print("Enter password to login: ");
+            String enteredPassword = input.nextLine();
+
+            boolean loginStatus = user.loginUser(enteredUsername, enteredPassword);
+            System.out.println(user.returnLoginStatus(loginStatus, firstName, lastName));
+        }
+
+        input.close();
+    }
+}
